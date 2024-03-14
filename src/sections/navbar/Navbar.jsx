72ffe7cbe9a4018a3e787275195551b1cceecad1
Ayush Chaurasia <ayushthegreat220@gmail.com>
2024-03-14 20:16:00 +0530

    Adding

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,36 +1,37 @@
-import React from 'react';
-import Logo from '../../assests/logo.jpg'
-import data from './data';
-import './navbar.css';
-import { links } from './../footer/data';
-
-
+import React from "react";
+import Logo from "../../assests/logo.jpg";
+import data from "./data";
+import "./navbar.css";
+import { links } from "./../footer/data";
 
 const Navbar = () => {
- 
   return (
     <nav>
-      <div className="container nav__container" >
-        <div className='logo'>
-        <a href="index.html" className='nav__logo '>
-          <img src={Logo} alt="Logo" />
+      <div className="container nav__container">
+        <div className="logo">
+          <a href="index.html" className="nav__logo ">
+            <img src={Logo} alt="Logo" />
           </a>
-          <div className='nav__text'>
-          <h4>Project</h4>
-          <h2>Assistant</h2>
-          <p>Development | Service | Consultancy</p>
+          <div className="nav__text">
+            <h4>Project</h4>
+            <h2>Assistance</h2>
+            <p>Development | Service | Consultancy</p>
           </div>
-          </div>
-         
+        </div>
+
         <ul className="nav__menu">
-          {
-             data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
-          }
+          {data.map((item) => (
+            <li key={item.id}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
         </ul>
-        <button className='btnm' href><a href="#services">Get Started</a></button>
+        <button className="btnm" href>
+          <a href="#services">Get Started</a>
+        </button>
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
